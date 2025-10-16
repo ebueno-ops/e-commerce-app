9a1000e0d3db525f832f443258a93df176f9db4b
ebueno-ops <84681248+ebueno-ops@users.noreply.github.com>
2025-10-15 21:01:40 -0600

    update product routes
    included edit and delete functionality

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,13 +1,14 @@
 import express from "express";
 import { getProducts } from "../controllers/ProductController.js";
 import {validateAdmin} from "../middleware/validateAdmin.js";
-import {addProduct} from "../models/ProductModel.js";
-import {editProduct} from "../models/ProductModel.js"
+import {addProduct,editProduct, deleteProduct} from "../models/ProductModel.js";
+
 
 
 const router = express.Router();
 
 router.get("/", getProducts);
+router.get("/admin-item-list", getProducts);
 
 //admin add product
 router.post("/add", validateAdmin, async (req,res) => {
@@ -21,20 +22,27 @@ router.post("/add", validateAdmin, async (req,res) => {
     });
 });
 
-// //admin edit
-router.post("/edit", validateAdmin, async (req,res) => {
+//admin edit
+router.put("/edit/:id", validateAdmin, async (req,res) => {
+    const {id} = req.params;
     const {name, price, promo_price, image} = req.body;
-    if (!name || !price || !image){
-        return res.json({ error: "Name, price, and image URL are required."});
-    }
-    addProduct({name, price, promo_price, image}, (err, result) =>{
+
+    editProduct({name, price, promo_price, image, id}, (err) =>{
         if (err) return res.json(err);
-        return res.json({message: "Product: " + name + " has been added", id: result.id });
+        return res.json({message: "Product: " + name + " edited successfully"});
     });
 });
-// //admin delete
-// router.delete();
-//
+
+//admin delete
+router.delete("/delete/:id", validateAdmin, async (req,res) => {
+    const {id} = req.params;
+
+    deleteProduct([id], (err) =>{
+        if (err) return res.json(err);
+        return res.json({message: "Product deleted successfully"});
+    });
+});
+
 // //seller set availability
 // router.put();
 
