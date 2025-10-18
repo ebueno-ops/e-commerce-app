63f2df984493646dff43f1d85a96aac2c25e1b58
Emmanuel Hombrebueno <emmanuel.hombrebueno@edu.sait.ca>
2025-10-17 21:33:42 -0600

    create seller dashboard

--- frontend/src/components/Seller/SellerDashboard.jsx
+++ frontend/src/components/Seller/SellerDashboard.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import {Button, Container} from "react-bootstrap";
+import SellerProductList from "./SellerProductList";
 
 function SellerDashboard() {
     return (
-        <div>
+        <Container>
             <h1>Seller Dashboard</h1>
-        </div>
+            <SellerProductList />
+        </Container>
     );
 }
 
